000182d13735537ad6919678185e0696acb532a5
bforbilly24 <halimputra0701@gmail.com>
2024-03-09 09:27:02 +0700

    feat(page): add /app/admin/layout

---
+++ src/app/admin/layout.js
@@ -0,0 +1,19 @@
+import { SessionProvider } from "@/components/provider/session-provider";
+import { Sidebar } from "@/components/navigation/sidebar";
+// import { authOptions } from "@/libs/auth-options";
+// import { getServerSession } from "next-auth";
+
+export const metadata = {
+	title: "Dashboard",
+};
+
+export default async function AdminLayout({ children }) {
+	// const session = await getServerSession(authOptions);
+
+	return (
+		<SessionProvider>
+			<Sidebar />
+			<div className="flex flex-1 flex-col justify-center gap-5 border-l p-8 sm:flex-row">{children}</div>
+		</SessionProvider>
+	);
+}
