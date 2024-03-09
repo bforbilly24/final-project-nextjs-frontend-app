09020b6bddac90ad853e4e5aa3cc58766ca01c10
bforbilly24 <halimputra0701@gmail.com>
2024-03-09 09:44:58 +0700

    feat(components): add /components/svg/command-mac-icon

---
+++ src/components/svg/command-mac-icon.js
@@ -0,0 +1,9 @@
+function CommandMacIcon() {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+			<path d="M18 3a3 3 0 00-3 3v12a3 3 0 003 3 3 3 0 003-3 3 3 0 00-3-3H6a3 3 0 00-3 3 3 3 0 003 3 3 3 0 003-3V6a3 3 0 00-3-3 3 3 0 00-3 3 3 3 0 003 3h12a3 3 0 003-3 3 3 0 00-3-3z"></path>
+		</svg>
+	);
+}
+
+export { CommandMacIcon };
