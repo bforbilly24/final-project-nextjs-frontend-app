91daf573293b2c2372300ac661dccd9c7d44ba7b
bforbilly24 <halimputra0701@gmail.com>
2024-03-18 22:32:46 +0700

    refac(page): remove useEffect, remove components DataTableViewOptions

--- src/app/admin/testing/page.js
+++ src/app/admin/testing/page.js
@@ -1,46 +1,12 @@
-'use client';
+'use client'
 import { DataTable } from '@/components/data-table/data-table';
 import { DataTableViewOptions } from '@/components/data-table/data-table-view-options';
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-
+import { useState } from 'react';
 
 export default function XmlDataTable() {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [visibleKeys, setVisibleKeys] = useState([]); // State to manage visible columns
-    const api = process.env.NEXT_PUBLIC_API_URL;
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(api);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch data');
-                }
-                const jsonData = await response.json();
-                setData(jsonData);
-                // Initialize visibleKeys with the first 12 keys
-                setVisibleKeys(Object.keys(jsonData?.c_akun[0] || {}).slice(0, 12));
-            } catch (error) {
-                setError(error.message);
-            }
-        };
-    
-        fetchData();
-    }, [api]);
-
-    // Filter data based on visibleKeys
-    const filteredData = data?.c_akun.map((item) => {
-        const filteredItem = {};
-        Object.keys(item).forEach((key) => {
-            if (visibleKeys.includes(key)) {
-                filteredItem[key] = item[key];
-            }
-        });
-        return filteredItem;
-    });
+    const [visibleKeys, setVisibleKeys] = useState([]);
 
     return (
         <div className='hidden h-full flex-1 flex-col gap-10 md:flex'>
@@ -50,18 +16,11 @@ export default function XmlDataTable() {
                     <div>Kembali ke beranda</div>
                 </Link>
                 <div>
-                    {error ? (
-                        <div>Error: {error}</div>
-                    ) : (
-                        <>
-                            <h2 className='text-2xl font-bold tracking-tight'>RKKS</h2>
-                            <p className='text-muted-foreground'>{`Ditemukan ${filteredData?.length ?? 0} entri data RKKS yang tersedia`}</p>
-                        </>
-                    )}
+                    <h2 className='text-2xl font-bold tracking-tight'>RKKS</h2>
+                    <p className='text-muted-foreground'>Data RKKS yang tersedia</p>
                 </div>
             </div>
-            <DataTableViewOptions allColumnHeaders={Object.keys(data?.c_akun[0] || {})} visibleKeys={visibleKeys} setVisibleKeys={setVisibleKeys} />
-            <DataTable data={filteredData ?? []} filterFocus={"name"} searchPlaceholder={"Cari nama..."} filters={ArrowLeftIcon} visibleKeys={visibleKeys} />
+            <DataTable filterFocus={"name"} searchPlaceholder={"Cari nama..."} filters={ArrowLeftIcon} visibleKeys={visibleKeys} />
         </div>
     );
 }
