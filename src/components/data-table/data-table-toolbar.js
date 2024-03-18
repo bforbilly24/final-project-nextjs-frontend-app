6418472faf765a19b2fe408208c9c1bc0a8125f5
bforbilly24 <halimputra0701@gmail.com>
2024-03-18 22:33:47 +0700

    refac(components); remove unused import

--- src/components/data-table/data-table-toolbar.js
+++ src/components/data-table/data-table-toolbar.js
@@ -6,19 +6,9 @@ import { Input } from '../shadcn/ui/input';
 import { DataTableFacetedFilter } from './data-table-faceted-filter';
 import { DataTableViewOptions } from './data-table-view-options';
 import { DataTableFilterOptions } from './data-table-filter-options';
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../shadcn/ui/dropdown-menu';
-import { MixerHorizontalIcon } from '@radix-ui/react-icons';
-import { formatCamelCase } from '@/libs/format-camel-case';
-import { useStore } from '@/libs/store';
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../shadcn/ui/dialog';
-// import { deleteAnnouncement } from "@/actions/delete-announcement";
-import { useToast } from '../shadcn/ui/use-toast';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 
-function DataTableToolbar({ data, filterFocus, searchPlaceholder, filters }) {
-    const [visibleKeys, setVisibleKeys] = useState([]);
 
+function DataTableToolbar({ data, filterFocus, searchPlaceholder, filters, visibleKeys, onViewOptionChange }) {
     // Dynamically generate allColumnHeaders from the received data
     const allColumnHeaders = data && data.length > 0 ? Object.keys(data[0]) : [];
 
@@ -37,10 +27,14 @@ function DataTableToolbar({ data, filterFocus, searchPlaceholder, filters }) {
         <div className='disabled:curs flex items-center justify-between'>
             <div className='flex flex-1 items-center space-x-5'>
                 <Input placeholder={searchPlaceholder} className='h-8 w-[150px] lg:w-[250px]' />
+                <Button className='h-8 px-2 lg:px-3'>
+                    <PlusCircledIcon className='mr-2 h-4 w-4' />
+                    Upload File .xml
+                </Button>
             </div>
             <div className='flex gap-5'>
                 <DataTableFilterOptions />
-                <DataTableViewOptions allColumnHeaders={allColumnHeaders} visibleKeys={visibleKeys} setVisibleKeys={setVisibleKeys} filteredData={filteredData} />
+                <DataTableViewOptions allColumnHeaders={allColumnHeaders} visibleKeys={visibleKeys} filteredData={filteredData} onViewOptionChange={onViewOptionChange} />
             </div>
         </div>
     );
