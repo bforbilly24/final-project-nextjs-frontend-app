8769aa065906f6203b4861ba6973c17fc73a9ffa
bforbilly24 <halimputra0701@gmail.com>
2024-03-24 01:57:45 +0700

    refactor(api): update fetch base url environment

--- src/app/admin/auth/api/page.js
+++ src/app/admin/auth/api/page.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const response = await fetch('http://localhost:8000/api/xml-data');
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL);
     const data = await response.json();
     res.status(200).json(data);
-  }
+}
