86cca0f3aa3a752d26330525efd9561e08e259e0
bforbilly24 <halimputra0701@gmail.com>
2024-03-24 01:55:54 +0700

    refactor(components): remove unused function, update mapping data

--- src/components/data-table/data-table-view-options.js
+++ src/components/data-table/data-table-view-options.js
@@ -4,105 +4,73 @@ import React, { useEffect, useState } from 'react';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuCheckboxItem } from '../shadcn/ui/dropdown-menu';
 import { Button } from '../shadcn/ui/button';
 import { EyeOpenIcon } from '@radix-ui/react-icons';
-import { DataTableToolbar } from './data-table-toolbar';
-import { ScrollArea } from '../shadcn/ui/scroll-area';
-import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from '../shadcn/ui/table';
-import { DataTablePagination } from './data-table-pagination';
-
-function DataTable({ filterFocus, searchPlaceholder, filters }) {
-	const [data, setData] = useState([]);
-	const [error, setError] = useState(null);
-	const [visibleKeys, setVisibleKeys] = useState([]);
-	const api = process.env.NEXT_PUBLIC_API_URL;
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(api);
-				if (!response.ok) {
-					throw new Error('Failed to fetch data');
-				}
-				const jsonData = await response.json();
-				setData(jsonData.c_akun);
-				setVisibleKeys(Object.keys(jsonData.c_akun[0] || {}).slice(0, 12));
-			} catch (error) {
-				setError(error.message);
-			}
-		};
-
-		fetchData();
-	}, [api]);
-
-	if (error) {
-		return <div>Error: {error}</div>;
-	}
-
-	if (!data || data.length === 0) {
-		return <div>No data available</div>;
-	}
-
-	const keys = Object.keys(data[0]);
-
-    const onViewOptionChange = (header, isChecked) => {
-        if (isChecked) {
-            setVisibleKeys(prevKeys => [...prevKeys, header]);
-        } else {
-            setVisibleKeys(prevKeys => prevKeys.filter(key => key !== header));
-        }
-    };
-
-    return (
-        <>
-            <DataTableViewOptions allColumnHeaders={keys} visibleKeys={visibleKeys} onViewOptionChange={onViewOptionChange} />
-            <DataTableToolbar data={data} filterFocus={filterFocus} searchPlaceholder={searchPlaceholder} filters={filters} visibleKeys={visibleKeys} />
-            <ScrollArea>{/* ... rest of the component ... */}</ScrollArea>
-            <DataTablePagination />
-        </>
-    );
-}
-
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '../shadcn/ui/drawer';
 
 function DataTableViewOptions({ allColumnHeaders, visibleKeys, filteredData, onViewOptionChange }) {
-    // Initialize checkedState
-    const [checkedState, setCheckedState] = useState(
-        allColumnHeaders.reduce((acc, curr) => ({ ...acc, [curr]: visibleKeys.includes(curr) }), {})
-    );
+	// Initialize checkedState
+	const [checkedState, setCheckedState] = useState(allColumnHeaders.reduce((acc, curr) => ({ ...acc, [curr]: visibleKeys.includes(curr) }), {}));
 
-    // Handler function
-    const handleCheckedChange = (header, isChecked) => {
-        setCheckedState(prevState => ({ ...prevState, [header]: isChecked }));
-        onViewOptionChange(header, isChecked);
-    };
+	// Handler function
+	const handleCheckedChange = (header, isChecked) => {
+		setCheckedState((prevState) => ({ ...prevState, [header]: isChecked }));
+		onViewOptionChange(header, isChecked);
+	};
 
 	useEffect(() => {
-        if (allColumnHeaders) {
-            const initialCheckedState = {};
-            allColumnHeaders.forEach(header => {
-                initialCheckedState[header] = visibleKeys.includes(header);
-            });
-            setCheckedState(initialCheckedState);
-        }
-    }, [visibleKeys, allColumnHeaders]);
-
+		if (allColumnHeaders) {
+			const initialCheckedState = {};
+			allColumnHeaders.forEach((header) => {
+				initialCheckedState[header] = visibleKeys.includes(header);
+			});
+			setCheckedState(initialCheckedState);
+		}
+	}, [visibleKeys, allColumnHeaders]);
 
 	// Render the component
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild className='disabled:pointer-events-auto disabled:cursor-not-allowed'>
-				<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex'>
+				<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex' onClick={(event) => {}}>
 					<EyeOpenIcon className='mr-2 h-4 w-4' />
 					Tampilan
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end' className='w-[180px]'>
-				<DropdownMenuLabel>Pilih tampilan</DropdownMenuLabel>
 				<DropdownMenuSeparator className='mb-1 mt-1' />
-                {allColumnHeaders &&
-                allColumnHeaders.map((header, index) => (
-                    <DropdownMenuCheckboxItem key={index} checked={checkedState[header]} onCheckedChange={(isChecked) => handleCheckedChange(header, isChecked)}>
-                        {header}
-                    </DropdownMenuCheckboxItem>
-                ))}
+				{allColumnHeaders && (
+					<Drawer>
+						<DrawerTrigger asChild>
+							<Button className='h-5'>
+								Lihat
+							</Button>
+						</DrawerTrigger>
+						<DrawerContent className={'hidden xl:flex'}>
+							<DrawerHeader>
+								<DrawerTitle>Kolom</DrawerTitle>
+								{/* {allColumnHeaders.map((header, index) => (
+									<div key={index}>
+										<input type=    'checkbox' checked={checkedState[header]} onChange={(e) => handleCheckedChange(header, e.target.checked)} />
+										{header}
+									</div>
+								))} */}
+								{allColumnHeaders && (
+									<div className='grid grid-cols-12 gap-2'>
+										{allColumnHeaders.map((header, index) => (
+											<DropdownMenuCheckboxItem key={index} checked={checkedState[header]} onCheckedChange={(isChecked) => handleCheckedChange(header, isChecked)}>
+												{header}
+											</DropdownMenuCheckboxItem>
+										))}
+									</div>
+								)}
+							</DrawerHeader>
+							<DrawerFooter>
+								<DrawerClose asChild>
+									<Button>Tutup</Button>
+								</DrawerClose>
+							</DrawerFooter>
+						</DrawerContent>
+					</Drawer>
+				)}
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
