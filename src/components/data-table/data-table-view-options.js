f8d38bfa84aaa20171b560cf865a66df8a1c7300
bforbilly24 <halimputra0701@gmail.com>
2024-03-28 17:02:47 +0700

    refactor(components): update view data show and hide using sheet and checkbox

--- src/components/data-table/data-table-view-options.js
+++ src/components/data-table/data-table-view-options.js
@@ -1,14 +1,13 @@
-'use client';
-
 import React, { useEffect, useState } from 'react';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuCheckboxItem } from '../shadcn/ui/dropdown-menu';
 import { Button } from '../shadcn/ui/button';
+import { Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle } from '../shadcn/ui/drawer';
+import { Checkbox } from '../shadcn/ui/checkbox'; // Import your custom Checkbox component
 import { EyeOpenIcon } from '@radix-ui/react-icons';
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '../shadcn/ui/drawer';
 
 function DataTableViewOptions({ allColumnHeaders, visibleKeys, filteredData, onViewOptionChange }) {
 	// Initialize checkedState
 	const [checkedState, setCheckedState] = useState(allColumnHeaders.reduce((acc, curr) => ({ ...acc, [curr]: visibleKeys.includes(curr) }), {}));
+	const [drawerOpen, setDrawerOpen] = useState(false); // Add this line
 
 	// Handler function
 	const handleCheckedChange = (header, isChecked) => {
@@ -28,51 +27,38 @@ function DataTableViewOptions({ allColumnHeaders, visibleKeys, filteredData, onV
 
 	// Render the component
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild className='disabled:pointer-events-auto disabled:cursor-not-allowed'>
-				<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex' onClick={(event) => {}}>
-					<EyeOpenIcon className='mr-2 h-4 w-4' />
-					Tampilan
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end' className='w-[180px]'>
-				<DropdownMenuSeparator className='mb-1 mt-1' />
-				{allColumnHeaders && (
-					<Drawer>
-						<DrawerTrigger asChild>
-							<Button className='h-5'>
-								Lihat
-							</Button>
-						</DrawerTrigger>
-						<DrawerContent className={'hidden xl:flex'}>
-							<DrawerHeader>
-								<DrawerTitle>Kolom</DrawerTitle>
-								{/* {allColumnHeaders.map((header, index) => (
-									<div key={index}>
-										<input type=    'checkbox' checked={checkedState[header]} onChange={(e) => handleCheckedChange(header, e.target.checked)} />
-										{header}
-									</div>
-								))} */}
-								{allColumnHeaders && (
-									<div className='grid grid-cols-12 gap-2'>
-										{allColumnHeaders.map((header, index) => (
-											<DropdownMenuCheckboxItem key={index} checked={checkedState[header]} onCheckedChange={(isChecked) => handleCheckedChange(header, isChecked)}>
-												{header}
-											</DropdownMenuCheckboxItem>
-										))}
-									</div>
-								)}
-							</DrawerHeader>
-							<DrawerFooter>
-								<DrawerClose asChild>
-									<Button>Tutup</Button>
-								</DrawerClose>
-							</DrawerFooter>
-						</DrawerContent>
-					</Drawer>
-				)}
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<div>
+			<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex' onClick={() => setDrawerOpen(true)}>
+				<EyeOpenIcon className='mr-2 h-4 w-4' />
+				Tampilan
+			</Button>
+			{allColumnHeaders && (
+				<Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+					<DrawerContent className={'hidden xl:flex'}>
+						<DrawerHeader>
+							<DrawerTitle>
+								<p className='mb-2 text-lg'>Kolom</p>
+							</DrawerTitle>
+							{allColumnHeaders && (
+								<div className='gap-y-2 grid grid-cols-12'>
+									{allColumnHeaders.map((header, index) => (
+										<div key={index} className='flex items-center justify-start'>
+											<Checkbox checked={checkedState[header]} onCheckedChange={(isChecked) => handleCheckedChange(header, isChecked)} />
+											<p className='ml-2'>{header}</p>
+										</div>
+									))}
+								</div>
+							)}
+						</DrawerHeader>
+						<DrawerFooter>
+							<DrawerClose asChild>
+								<Button onClick={() => setDrawerOpen(false)}>Tutup</Button>
+							</DrawerClose>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
+			)}
+		</div>
 	);
 }
 
