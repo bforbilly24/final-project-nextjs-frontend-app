0519c69d62ba96daff5229b8b108e297d8e36375
bforbilly24 <halimputra0701@gmail.com>
2024-03-31 02:14:59 +0700

    refactor(components): add button reset for search

--- src/components/data-table/data-table-toolbar.js
+++ src/components/data-table/data-table-toolbar.js
@@ -8,35 +8,46 @@ import { DataTableViewOptions } from './data-table-view-options';
 import { DataTableFilterOptions } from './data-table-filter-options';
 
 function DataTableToolbar({ data, filterFocus, searchPlaceholder, filters, visibleKeys, onViewOptionChange, searchTerm, onSearchChange }) {
-    // Dynamically generate allColumnHeaders from the received data
-    const allColumnHeaders = data && data.length > 0 ? Object.keys(data[0]) : [];
+	const allColumnHeaders = data && data.length > 0 ? Object.keys(data[0]) : [];
 
-    // Filter data based on visibleKeys
-    const filteredData = data?.map((item) => {
-        const filteredItem = {};
-        Object.keys(item).forEach((key) => {
-            if (visibleKeys.includes(key)) {
-                filteredItem[key] = item[key];
-            }
-        });
-        return filteredItem;
-    });
+	// Filter data based on visibleKeys
+	const filteredData = data?.map((item) => {
+		const filteredItem = {};
+		Object.keys(item).forEach((key) => {
+			if (visibleKeys.includes(key)) {
+				filteredItem[key] = item[key];
+			}
+		});
+		return filteredItem;
+	});
 
-    return (
-        <div className='disabled:curs flex items-center justify-between'>
-            <div className='flex flex-1 items-center space-x-5'>
-                <Input placeholder={searchPlaceholder} className='h-8 w-[150px] lg:w-[250px]' value={searchTerm} onChange={(event) => onSearchChange(event.target.value)} />
-                <Button className='h-8 px-2 lg:px-3'>
-                    <PlusCircledIcon className='mr-2 h-4 w-4' />
-                    Upload File .xml
-                </Button>
-            </div>
-            <div className='flex gap-5'>
-                {/* <DataTableFilterOptions /> */}
-                <DataTableViewOptions allColumnHeaders={allColumnHeaders} visibleKeys={visibleKeys} filteredData={filteredData} onViewOptionChange={onViewOptionChange} />
-            </div>
-        </div>
-    );
+	const isFiltered = searchTerm !== '';
+
+	const resetSearch = () => {
+		onSearchChange('');
+	};
+
+	return (
+		<div className='disabled:curs flex items-center justify-between'>
+			<div className='flex flex-1 items-center space-x-5'>
+				<Input placeholder={searchPlaceholder} className='h-8 w-[150px] lg:w-[250px]' value={searchTerm} onChange={(event) => onSearchChange(event.target.value)} />
+				{isFiltered && (
+					<Button onClick={resetSearch} className='h-8 px-3'>
+						Reset
+						<Cross2Icon className='ml-1 h-4 w-4' />
+					</Button>
+				)}
+				<Button className='h-8 px-2 lg:px-3'>
+					<PlusCircledIcon className='mr-2 h-4 w-4' />
+					Upload File .xml
+				</Button>
+			</div>
+			<div className='flex gap-5'>
+				{/* <DataTableFilterOptions /> */}
+				<DataTableViewOptions allColumnHeaders={allColumnHeaders} visibleKeys={visibleKeys} filteredData={filteredData} onViewOptionChange={onViewOptionChange} />
+			</div>
+		</div>
+	);
 }
 
 export { DataTableToolbar };
