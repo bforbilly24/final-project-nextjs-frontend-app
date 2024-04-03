739a25afc6a96ceb39ae6356e7cd98cd45b9c0b5
bforbilly24 <halimputra0701@gmail.com>
2024-04-03 23:06:21 +0700

    refactor(page): update meta data, update Data Table

--- src/app/admin/rkks/page.js
+++ src/app/admin/rkks/page.js
@@ -1,24 +1,25 @@
 // import { getServerSession } from 'next-auth/next';
 // import { authOptions } from '@/libs/auth-options';
 // import { UnauthorizedContent } from '@/components/miscellaneous/unauthorized-content';
-// import { getPackage } from '@/actions/get-package';
+import { getPackage } from '@/actions/get-package';
 import Link from 'next/link';
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import { DataTable } from '@/components/data-table/data-table';
 import { packageColumns } from '@/utils/column';
-// import { getService } from '@/actions/get-service';
+import { getService } from '@/actions/get-service';
 
 export const metadata = {
-	title: 'Paket',
+	title: 'RKKS Data',
 };
 
-export default async function PaketPage() {
-// 	const session = await getServerSession(authOptions);
 
-// 	if (!session) return <UnauthorizedContent />;
+export default async function RkksPage() {
+    // const session = await getServerSession(authOptions);
 
-// 	const packages = await getPackage();
-// 	const services = await getService();
+    // if (!session) return <UnauthorizedContent />;
+
+    const packages = await getPackage();
+    const services = await getService();
 
 	return (
 		<div className='hidden h-full flex-1 flex-col gap-10 md:flex'>
@@ -28,11 +29,11 @@ export default async function PaketPage() {
 					<div>Kembali ke beranda</div>
 				</Link>
 				<div>
-					<h2 className='text-2xl font-bold tracking-tight'>Paket</h2>
-					<p className='text-muted-foreground'>{`Ditemukan 0 entri paket yang tersedia`}</p>
+					<h2 className='text-2xl font-bold tracking-tight'>Data XML</h2>
+					<p className='text-muted-foreground'>{`Ditemukan ${packages?.data?.length} entri paket yang tersedia`}</p>
 				</div>
 			</div>
-			<DataTable filterFocus={'name'} searchPlaceholder={'Nama...'} filters={['code']} />
+			<DataTable data={packages} columns={packageColumns} filterFocus={'name'} searchPlaceholder={'Nama...'} filters={['code']} services={services} />
 		</div>
 	);
 }
