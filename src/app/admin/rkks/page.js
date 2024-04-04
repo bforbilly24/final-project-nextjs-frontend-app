6a2e30cc46e52dfe0de1575909f96cf810992259
bforbilly24 <halimputra0701@gmail.com>
2024-04-05 01:09:49 +0700

    refactor(page): remove any unused import

--- src/app/admin/rkks/page.js
+++ src/app/admin/rkks/page.js
@@ -1,12 +1,10 @@
 // import { getServerSession } from 'next-auth/next';
 // import { authOptions } from '@/libs/auth-options';
 // import { UnauthorizedContent } from '@/components/miscellaneous/unauthorized-content';
-import { getPackage } from '@/actions/get-package';
 import Link from 'next/link';
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import { DataTable } from '@/components/data-table/data-table';
 import { packageColumns } from '@/utils/column';
-import { getService } from '@/actions/get-service';
 
 export const metadata = {
 	title: 'RKKS Data',
@@ -18,9 +16,6 @@ export default async function RkksPage() {
 
     // if (!session) return <UnauthorizedContent />;
 
-    const packages = await getPackage();
-    const services = await getService();
-
 	return (
 		<div className='hidden h-full flex-1 flex-col gap-10 md:flex'>
 			<div className='flex flex-col justify-between gap-6 space-y-2'>
@@ -30,10 +25,9 @@ export default async function RkksPage() {
 				</Link>
 				<div>
 					<h2 className='text-2xl font-bold tracking-tight'>Data XML</h2>
-					<p className='text-muted-foreground'>{`Ditemukan ${packages?.data?.length} entri paket yang tersedia`}</p>
 				</div>
 			</div>
-			<DataTable data={packages} columns={packageColumns} filterFocus={'name'} searchPlaceholder={'Nama...'} filters={['code']} services={services} />
+			<DataTable columns={packageColumns} filterFocus={'name'} searchPlaceholder={'Nama...'} filters={['code']}  />
 		</div>
 	);
 }
