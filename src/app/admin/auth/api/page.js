9fc6441d340bada177efceec637ac1bdc0abaac6
bforbilly24 <halimputra0701@gmail.com>
2024-04-15 20:03:37 +0700

    refactor(file): remove any unused

--- src/app/admin/auth/api/page.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL);
-    const data = await response.json();
-    res.status(200).json(data);
-}
