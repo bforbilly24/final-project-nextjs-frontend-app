69dae452a2263be74ee8341a5a07d320ab12b46f
bforbilly24 <halimputra0701@gmail.com>
2024-06-03 20:48:11 +0700

    feat(api): add /api/auth/[..nextauth]/route

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,7 @@
+// src/app/admin/api/auth/[...nextauth]/route.js
+import NextAuth from 'next-auth';
+import { authOptions } from '@/libs/auth-options';
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
