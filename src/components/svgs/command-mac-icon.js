efc184ccf35ab3a8493055254eed24833e484e96
bforbilly24 <halimputra0701@gmail.com>
2024-06-04 02:28:01 +0700

    feat(component): add /svgs/command-mac-icon

---
+++ src/components/svgs/command-mac-icon.js
@@ -0,0 +1,12 @@
+function CommandMacIcon({ className }) {
+	return (
+		// <svg xmlns='http://www.w3.org/2000/svg' width='36' height='36' fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24'>
+		// 	<path d='M18 3a3 3 0 00-3 3v12a3 3 0 003 3 3 3 0 003-3 3 3 0 00-3-3H6a3 3 0 00-3 3 3 3 0 003 3 3 3 0 003-3V6a3 3 0 00-3-3 3 3 0 00-3 3 3 3 0 003 3h12a3 3 0 003-3 3 3 0 00-3-3z'></path>
+		// </svg>
+		<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' style={{ msFilter: '' }} className={className}>
+			<path d='M6 14c-2.206 0-4 1.794-4 4s1.794 4 4 4a4.003 4.003 0 003.998-3.98H10V16h4v2.039h.004A4.002 4.002 0 0018 22c2.206 0 4-1.794 4-4s-1.794-4-4-4h-2v-4h2c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4v2h-4V5.98h-.002A4.003 4.003 0 006 2C3.794 2 2 3.794 2 6s1.794 4 4 4h2v4H6zm2 4c0 1.122-.879 2-2 2s-2-.878-2-2 .879-2 2-2h2v2zm10-2c1.121 0 2 .878 2 2s-.879 2-2 2-2-.878-2-2v-2h2zM16 6c0-1.122.879-2 2-2s2 .878 2 2-.879 2-2 2h-2V6zM6 8c-1.121 0-2-.878-2-2s.879-2 2-2 2 .878 2 2v2H6zm4 2h4v4h-4v-4z'></path>
+		</svg>
+	);
+}
+
+export { CommandMacIcon };
