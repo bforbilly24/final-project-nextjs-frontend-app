d10d81e69c5a10c759d39912eb1a1f3c7d324c2f
bforbilly24 <halimputra0701@gmail.com>
2024-06-06 01:30:24 +0700

    refactor(page): update import, add session, remove unused comment

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,17 +1,24 @@
 import { Badge } from '@/components/shadcn/ui/badge';
 import { Card } from '@/components/shadcn/ui/card';
 import { RouterButton } from '@/components/button/router-button';
-import { ToastButton } from '@/components/button/toast-button';
-import { LogoutButton } from '@/components/button/logout-button';
 import { ToggleTheme } from '@/hooks/toggle-theme';
-import authOptions from '@/libs/auth-options';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
+import { authOptions } from '@/libs/auth-options';
+import { LogoutButton } from '@/components/button/logout-button';
+import { ToastButton } from '@/components/button/toast-button';
+
+export const metadata = {
+	title: 'Admin',
+};
 
 export default async function AdminPage() {
-	// const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
-	// if (!session) return redirect('/admin/auth/login');
+	if (!session) {
+		redirect('/admin/auth/login');
+		return null;
+	}
 
 	return (
 		<div className='flex w-fit flex-1 flex-col gap-5'>
@@ -42,96 +49,43 @@ export default async function AdminPage() {
 						</div>
 					</Card>
 
-					{/* <Card>
-						<div className="h-28 w-full cursor-pointer">
-							<div className="flex h-full gap-5 p-6">
-								<div className="flex h-full w-20 items-center justify-center rounded bg-primary">
-									<i className="bx bx-command text-2xl text-white"></i>
-								</div>
-								<div className="flex flex-col justify-center gap-2">
-									<div className="text-muted-foreground">Documentation</div>
-									<div className="text-primary-800">Discover the essential concepts, guides and instructions.</div>
-								</div>
-							</div>
-						</div>
-					</Card> */}
-
-					{/* <Card>
-						<div className="flex w-full gap-5 p-6">
-							<GeneratePasswordButton />
-							<ComparePasswordButton />
-						</div>
-					</Card> */}
-
 					<Card>
 						<div className='flex w-full gap-5 p-6'>
-							{/* <RouterButton text={'Pengumuman'} action={'/admin/pengumuman'} /> */}
 							<RouterButton text={'RKKS'} action={'/admin/rkks'} />
-							{/* <RouterButton text={'Paket'} action={'/admin/paket'} />
-							<RouterButton text={'Penyedia'} action={'/admin/penyedia'} />
-							<RouterButton text={'Tenaga Ahli'} action={'/admin/tenaga-ahli'} />
-							<RouterButton text={'Peralatan'} action={'/admin/peralatan'} />
-							<RouterButton text={'Pengguna'} action={'/admin/pengguna'} /> */}
 						</div>
 					</Card>
 
 					<Card>
-						<ToggleTheme />
-
-						{/* <div className="flex w-full gap-5 p-6">
-							<RouterButton text={"Profil"} action={"/admin/profil"} />
-							<RouterButton text={"Pengaturan"} action={"/admin/pengaturan"} />
-						</div> */}
-					</Card>
-					{/* 
-					<Card>
 						<div className='flex w-full gap-5 p-6'>
-							<ToastButton title={'Authenticated'} description={'test'} text={'Informasi'} />
+							<ToastButton title={'Authenticated'} description={JSON.stringify(session, null, 4)} text={'Informasi'} />
 							<LogoutButton />
 						</div>
-					</Card> */}
+					</Card>
 				</div>
-
-				{/* <div className='flex h-full w-3/6 flex-col '> */}
-				{/* <Card>
-						<div className="relative h-28 w-full cursor-pointer">
-							<div className="flex h-full gap-5 p-6 w-full">
-								<div className="flex h-full w-full items-center justify-center rounded bg-primary">
-									<i className="bx bxl-visual-studio text-2xl text-white"></i>
-								</div>
-								<div className="flex h-full w-full items-center justify-center rounded bg-primary">
-									<i className="bx bxl-javascript text-2xl text-white"></i>
-								</div>
-								<div className="flex h-full w-full items-center justify-center rounded bg-primary">
-									<i className="bx bxl-tailwind-css text-2xl text-white"></i>
-								</div>
-								
-							</div>
-						</div>
-					</Card> */}
-
-				{/* <Card className='h-full'>
+				<div className='flex h-full w-3/6 flex-col '>
+					<Card className='h-full'>
 						<div className='flex h-full flex-col justify-between gap-5 p-6'>
 							<div className='flex h-full w-full flex-col justify-center gap-2 rounded-md bg-destructive p-6 text-white hover:cursor-text'>
 								<div className='font-mono text-xs'>
-									<div>id:</div>1
+									<div>id:</div>
+									{session.user.id}
 								</div>
 								<div className='font-mono text-xs'>
 									<div>username:</div>
-									Super Admin
+									{session.user.username}
 								</div>
 								<div className='font-mono text-xs'>
 									<div>name:</div>
-									Circle IT
+									{session.user.name}
 								</div>
 								<div className='font-mono text-xs'>
 									<div>email:</div>
-									circleitwarehouse@cit.id
+									{session.user.email}
 								</div>
 							</div>
 						</div>
-					</Card> */}
-				{/* </div> */}
+					</Card>
+				</div>
 			</div>
 		</div>
 	);
