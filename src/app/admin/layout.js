e695b3ea9fbd63be1f5a47bfd76465c5059fe873
bforbilly24 <halimputra0701@gmail.com>
2024-06-06 01:33:50 +0700

    refactor(layout): add session

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -8,13 +8,12 @@ export const metadata = {
 };
 
 export default async function AdminLayout({ children }) {
-	// const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
 	return (
 		<SessionProvider>
-			{/* {session && <Sidebar />} */}
-			<Sidebar />
-			<div className='flex flex-1 flex-col justify-center gap-5 border-l p-8 sm:flex-row'>{children}</div>
+			{session && <Sidebar />}
+			<div className='flex w-full flex-1 flex-col justify-center gap-5 border-l p-8 sm:flex-row'>{children}</div>
 		</SessionProvider>
 	);
 }
