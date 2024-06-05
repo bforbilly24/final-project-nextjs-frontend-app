de40c18a4e725c0351126c82a1aa62db2d118c0a
bforbilly24 <halimputra0701@gmail.com>
2024-06-06 03:28:33 +0700

    feat(util): add author

---
+++ src/utils/authors.js
@@ -0,0 +1,14 @@
+const authors = {
+	id: 'rkkspens-nextjs-fullstack-dashboard',
+	displayName: 'RKKS - Rencana Kerja Kegiatan Satuan',
+	version: '1.0.1',
+	versionCode: '100',
+	private: true,
+	author: {
+		name: 'Halim Putra',
+		email: 'halimputra0701@icloud.com',
+		url: 'https://bforbilly.me/',
+	},
+};
+
+export { authors };
