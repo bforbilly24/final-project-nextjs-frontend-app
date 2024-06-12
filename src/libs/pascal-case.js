6b09cc3e8cda7923c75c0cb30eb9bf1f13298af7
bforbilly24 <halimputra0701@gmail.com>
2024-06-13 00:18:38 +0700

    feat(lib): add pascal-case

---
+++ src/libs/pascal-case.js
@@ -0,0 +1,8 @@
+function PascalCase(snakeCaseString) {
+	return snakeCaseString
+		.split('_')
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+		.join(' ');
+}
+
+export { PascalCase };
