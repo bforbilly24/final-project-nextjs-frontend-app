51c04f6c34e588ec90c1911c5d1b109e8be1d6b8
bforbilly24 <halimputra0701@gmail.com>
2024-06-13 00:09:53 +0700

    feat(component): add /data-table/data-table-faceted-filter

--- src/components/data-table/data-table-faceted-filter.js
+++ src/components/data-table/data-table-faceted-filter.js
@@ -1,95 +1,92 @@
+// src/components/data-table/data-table-faceted-filter.js
 import { Popover, PopoverTrigger, PopoverContent } from "../shadcn/ui/popover";
 import { Button } from "../shadcn/ui/button";
-import { PlusCircledIcon, CheckIcon } from "@radix-ui/react-icons";
-import { formatCamelCase } from "@/libs/format-camel-case";
+import { PlusCircledIcon, ExclamationTriangleIcon, CheckIcon } from "@radix-ui/react-icons";
 import { Separator } from "../shadcn/ui/separator";
 import { Badge } from "../shadcn/ui/badge";
-import { removeDuplicate } from "@/libs/remove-duplicate";
-import { Command, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator } from "../shadcn/ui/command";
+import { Command, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator } from "../shadcn/ui/command";
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogTitle } from '../shadcn/ui/alert-dialog';
 import { cn } from "@/utils/utils";
+import { useState } from 'react';
 
-function DataTableFacetedFilter({ column, title, options, optionName }) {
-	const facets = column?.getFacetedUniqueValues();
-	const selectedValues = new Set(column?.getFilterValue());
+const sortOptions = [
+    { name: "Ascending", value: "asc" },
+    { name: "Descending", value: "desc" }
+];
 
-	return (
-		<Popover>
-			<PopoverTrigger asChild>
-				<Button variant="outline" size="sm" className="h-8">
-					<PlusCircledIcon className="mr-2 h-4 w-4" />
-					{title == "Npwp" || title == "Nik" || title == "Pagu" ? title.toUpperCase() : title == "DurationOfService" || title == "MachineNumber" || title == "LicensePlate" || title == "ManufactureYear" || title == "ReceiptNumber" ? formatCamelCase(title) : title}
-					{selectedValues?.size > 0 && (
-						<>
-							<Separator orientation="vertical" className="mx-2 h-4" />
-							<Badge variant="secondary" className="rounded-sm px-1 font-normal lg:hidden">
-								{selectedValues.size}
-							</Badge>
-							<div className="hidden space-x-1 lg:flex">
-								{selectedValues.size > 2 ? (
-									<Badge variant="secondary" className="rounded-sm px-1 font-normal">
-										{selectedValues.size} selected
-									</Badge>
-								) : (
-									removeDuplicate(options, optionName)
-										.filter((option) => selectedValues.has(option[optionName]))
-										.map((option, index) => (
-											<Badge variant="secondary" key={index} className="rounded-sm px-1 font-normal">
-												{option[optionName]}
-											</Badge>
-										))
-								)}
-							</div>
-						</>
-					)}
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-[200px] p-0" align="start">
-				<Command>
-					<CommandInput
-						placeholder={title == "Npwp" || title == "Nik" || title == "Pagu" ? title.toUpperCase() : title == "DurationOfService" || title == "MachineNumber" || title == "LicensePlate" || title == "ManufactureYear" || title == "ReceiptNumber" ? formatCamelCase(title) : title}
-					/>
-					<CommandList>
-						<CommandEmpty>No results found.</CommandEmpty>
-						<CommandGroup>
-							{removeDuplicate(options, optionName).map((option, index) => {
-								const isSelected = selectedValues.has(option[optionName]);
-								return (
-									<CommandItem
-										key={index}
-										onSelect={() => {
-											if (isSelected) {
-												selectedValues.delete(option[optionName]);
-											} else {
-												selectedValues.add(option[optionName]);
-											}
-											const filterValues = Array.from(selectedValues);
-											column?.setFilterValue(filterValues.length ? filterValues : undefined);
-										}}
-									>
-										<div className={cn("mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary", isSelected ? "bg-primary text-primary-foreground" : "opacity-50 [&_svg]:invisible")}>
-											<CheckIcon className={cn("h-4 w-4")} />
-										</div>
-										<span className="truncate">{option[optionName]}</span>
-										{facets?.get(option[optionName]) && <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">{facets.get(option[optionName])}</span>}
-									</CommandItem>
-								);
-							})}
-						</CommandGroup>
-						{selectedValues.size >= 0 && (
-							<>
-								<CommandSeparator />
-								<CommandGroup>
-									<CommandItem onSelect={() => column?.setFilterValue(undefined)} className="justify-center text-center">
-										Hapus filter
-									</CommandItem>
-								</CommandGroup>
-							</>
-						)}
-					</CommandList>
-				</Command>
-			</PopoverContent>
-		</Popover>
-	);
+function DataTableFacetedFilter({ column, title, sortOrder, onSortChange }) {
+    const [alertOpen, setAlertOpen] = useState(false);
+    const [checkedState, setCheckedState] = useState(
+        sortOptions.reduce((acc, option) => {
+            acc[option.value] = sortOrder === option.value;
+            return acc;
+        }, {})
+    );
+
+    const handleSortChange = (value) => {
+        const newCheckedState = { ...checkedState, [value]: !checkedState[value] };
+        const newSortOrder = newCheckedState[value] ? value : undefined;
+        setCheckedState(newCheckedState);
+        onSortChange(newSortOrder);
+    };
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="outline" size="sm" className="h-8">
+                    <PlusCircledIcon className="mr-2 h-4 w-4" />
+                    {title}
+                    {sortOrder && (
+                        <>
+                            <Separator orientation="vertical" className="mx-2 h-4" />
+                            <Badge variant="secondary" className="rounded-sm px-1 font-normal">
+                                {sortOrder.charAt(0).toUpperCase() + sortOrder.slice(1)}
+                            </Badge>
+                        </>
+                    )}
+                    Filter
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0" align="start">
+                <Command>
+                    <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup>
+                            {sortOptions.map((option, index) => (
+                                <CommandItem
+                                    key={index}
+                                    onSelect={() => handleSortChange(option.value)}
+                                >
+                                    <div className={cn("mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary", checkedState[option.value] ? "bg-primary text-primary-foreground" : "opacity-50 [&_svg]:invisible")}>
+                                        <CheckIcon className={cn("h-4 w-4")} />
+                                    </div>
+                                    <span className="truncate">{option.name}</span>
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                        <CommandSeparator />
+                        <CommandGroup>
+                            <CommandItem onSelect={() => handleSortChange(undefined)} className="justify-center text-center">
+                                Clear sorting
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+            <AlertDialog open={alertOpen} onOpenChange={setAlertOpen}>
+                <AlertDialogContent>
+                    <AlertDialogTitle className='flex items-center gap-x-2'>
+                        <ExclamationTriangleIcon className='h-4 w-4' />
+                        <p>Peringatan</p>
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>Uncheck terlebih dahulu untuk memilih filter yang diinginkan.</AlertDialogDescription>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel onClick={() => setAlertOpen(false)}>Close</AlertDialogCancel>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </Popover>
+    );
 }
 
 export { DataTableFacetedFilter };
