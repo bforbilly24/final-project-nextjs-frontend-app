f97ae03955964c34e6005f37a75cced2d0eabd7e
bforbilly24 <halimputra0701@gmail.com>
2024-06-13 00:23:53 +0700

    style(page): update styling

--- src/app/admin/rkks/page.js
+++ src/app/admin/rkks/page.js
@@ -15,14 +15,14 @@ export default async function RkksPage() {
 	if (!session) return <UnauthenticatedContent />;
 
 	return (
-		<div className='hidden h-ful w-full flex-1 flex-col gap-10 md:flex'>
+		<div className='hidden h-full w-full flex-1 flex-col gap-10 md:flex'>
 			<div className='flex flex-col justify-between gap-6 space-y-2'>
 				<Link href='/admin' className='flex w-fit cursor-pointer items-center gap-5 text-primary focus:outline-none'>
 					<ArrowLeftIcon className='h-5 w-5 cursor-pointer' />
 					<div>Kembali ke beranda</div>
 				</Link>
 				<div>
-					<h2 className='text-2xl font-bold tracking-tight'>Data XML</h2>
+					<h2 className='text-2xl font-bold tracking-tight'>Data RKKS</h2>
 				</div>
 			</div>
 			<DataTable filterFocus={'name'} searchPlaceholder={'Search...'} filters={['code']} />
